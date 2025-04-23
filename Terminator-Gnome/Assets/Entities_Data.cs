e57e5567579720e1a94592e585d829f42db04416
JerePR35 <jerepadoan@gmail.com>
2025-04-23 19:31:55 -0300

    Prefab Enemy and Class enemy
    Added more data to enemy data and create prefab of enemy melee

--- Terminator-Gnome/Assets/Entities_Data.cs
+++ Terminator-Gnome/Assets/Entities_Data.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 public class Entities_Data : ScriptableObject
 {
     [SerializeField] private float _movementSpeed;
+    [SerializeField] private float _maxHealth;
+    [SerializeField] private float _attackDamage;
+    [SerializeField] private float _attackSpeed;
 
     public float movementSpeed => _movementSpeed;
+    public float maxHealth => _maxHealth;
+    public float attackDamage => _attackDamage;
+    public float attackSpeed => _attackSpeed;
+
 }

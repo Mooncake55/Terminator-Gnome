bf905e347fa0f64b539ef78c4213cd34a697f34a
NeoExtrass <106704364+ColoGamboa@users.noreply.github.com>
2025-04-23 19:34:32 -0300

    Dash
    implementación del dash (no funciona)

--- Terminator-Gnome/Assets/Scripts/InputController.cs
+++ Terminator-Gnome/Assets/Scripts/InputController.cs
@@ -8,6 +8,7 @@ public class InputController : MonoBehaviour
 {
     public static InputController Instance;
     public event Action<Vector2> OnMoveInput;
+    public event Action<bool> OnShiftPressed;
 
     void Awake()
     {
@@ -15,13 +16,14 @@ public class InputController : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        //Debug.Log("Hello");
         Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        if(moveInput != Vector2.zero)
-        {
-            Debug.Log("Should Move");
-            OnMoveInput?.Invoke(moveInput.normalized);
+        OnMoveInput?.Invoke(moveInput.normalized);
+        if (Input.GetKey(KeyCode.LeftShift))
+        { 
+            OnShiftPressed?.Invoke(true);
+            Debug.Log("Shift");
         }
     }
+
 
 }
--- Terminator-Gnome/Assets/Scripts/PlayerMovement.cs
+++ Terminator-Gnome/Assets/Scripts/PlayerMovement.cs
@@ -5,32 +5,63 @@ public class PlayerMovement : MonoBehaviour
 {
     int moveCount = 0;
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float dashSpeed = 5f;
+    [SerializeField] private float dashDuration = 0.2f;
+    private Vector2 lastDirection = Vector2.zero;
     private Rigidbody2D rb;
     private bool isPaused = false;
+
+    bool isDashing = false;
     
     void Start()
     {
+        GetComponent<Rigidbody2D>().gravityScale = 0f;
         rb = GetComponent<Rigidbody2D>();
         InputController.Instance.OnMoveInput += HandleMoveInput;
+        InputController.Instance.OnShiftPressed += HandleDash;
     }
 
-    void MovePlayer(Vector2 moveInput)
+    void MovePlayer(Vector2 direction)
     {
-        Debug.Log($"Horizontal: " + moveInput.x);
-        Debug.Log($"Vertical: " + moveInput.y);
-        moveCount++;
-        Debug.Log($"move count" + moveCount);
-        rb.velocity = moveInput * speed; //rb.volocity
-        moveInput = Vector2.zero;
+        //Debug.Log($"Horizontal: " + moveInput.x);
+        //Debug.Log($"Vertical: " + moveInput.y);
+        //moveCount++;
+        //Debug.Log($"move count" + moveCount);
+        float originalSpeed = speed;
+        if (isDashing)
+        {
+            Dash(direction, false);
+            return;
+        }
+        rb.linearVelocity = direction * speed;
+        speed = originalSpeed;
+        isDashing = false;
+        //moveInput = Vector2.zero;
     }
     void HandleMoveInput(Vector2 moveInput)
     {
         if (isPaused) return;
+        lastDirection = moveInput;
+        MovePlayer(lastDirection);
+    }
+    void HandleDash(bool dash)
+    {
+        if (dash)
+        {
+            Dash(lastDirection, false) ; return;
+        }
         else 
         {
-            MovePlayer(moveInput);
-            //moveInput = Vector2.zero;
+            Dash(lastDirection, true) ; return;
         }
+    }
+
+    System.Collections.IEnumerator Dash(Vector2 direction, bool changeDirection)
+    {
+        if (changeDirection) { direction = -direction; }
+        rb.linearVelocity = direction * (speed + dashSpeed);
+        yield return new WaitForSeconds(dashDuration);
+        isDashing = false;
     }
 
 }

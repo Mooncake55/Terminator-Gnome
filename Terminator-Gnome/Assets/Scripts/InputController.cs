ffaf20013ed4449cc5427a0cc03d8342e2e9e3c5
Martin Gamboa <106704364+ColoGamboa@users.noreply.github.com>
2025-04-27 15:38:16 -0300

    fixed dash and movement
    el dash se implementa como un movimiento instantáneo de un punto A a un punto B

--- Terminator-Gnome/Assets/Scripts/InputController.cs
+++ Terminator-Gnome/Assets/Scripts/InputController.cs
@@ -8,22 +8,37 @@ public class InputController : MonoBehaviour
 {
     public static InputController Instance;
     public event Action<Vector2> OnMoveInput;
-    public event Action<bool> OnShiftPressed;
+    public event Action<Vector2, bool> OnShiftPressed;
 
     void Awake()
     {
         Instance = this;
     }
-    private void FixedUpdate()
+    private void Update()
     {
-        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        OnMoveInput?.Invoke(moveInput.normalized);
+        //Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        //OnMoveInput?.Invoke(moveInput.normalized);
+        //if (Input.GetKey(KeyCode.LeftShift))
+        //{ 
+        //    OnShiftPressed?.Invoke(moveInput.normalized, true);
+        //    Debug.Log("Shift");
+        //}
+        bool isLeftShiftPressed = false;
         if (Input.GetKey(KeyCode.LeftShift))
-        { 
-            OnShiftPressed?.Invoke(true);
-            Debug.Log("Shift");
+        {
+            isLeftShiftPressed = true;
         }
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (isLeftShiftPressed)
+        {
+            OnShiftPressed?.Invoke(moveInput.normalized, true);
+
+        }
+        else
+        {
+            OnMoveInput?.Invoke(moveInput.normalized);
+        }
+
+
     }
-
-
 }
--- Terminator-Gnome/Assets/Scripts/PlayerMovement.cs
+++ Terminator-Gnome/Assets/Scripts/PlayerMovement.cs
@@ -1,32 +1,31 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    int moveCount = 0;
-    [SerializeField] private float speed = 5f;
-    [SerializeField] private float dashSpeed = 5f;
-    [SerializeField] private float dashDuration = 0.2f;
+    [SerializeField] private float speed = 5f; 
+    [SerializeField] private float dashSpeed = 1f;//also dashDistance
+    [SerializeField] private float dashDuration = 0.2f; //dash normal
+    private int dashCount = 2;
+    public LayerMask obstacleLayer;
+    private bool isDashing = false;//dash normal
+
     private Vector2 lastDirection = Vector2.zero;
     private Rigidbody2D rb;
     private bool isPaused = false;
-
-    bool isDashing = false;
     
     void Start()
     {
-        GetComponent<Rigidbody2D>().gravityScale = 0f;
+        //GetComponent<Rigidbody2D>().gravityScale = 0f;
         rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0f;
         InputController.Instance.OnMoveInput += HandleMoveInput;
         InputController.Instance.OnShiftPressed += HandleDash;
     }
 
     void MovePlayer(Vector2 direction)
     {
-        //Debug.Log($"Horizontal: " + moveInput.x);
-        //Debug.Log($"Vertical: " + moveInput.y);
-        //moveCount++;
-        //Debug.Log($"move count" + moveCount);
         float originalSpeed = speed;
         if (isDashing)
         {
@@ -44,24 +43,84 @@ public class PlayerMovement : MonoBehaviour
         lastDirection = moveInput;
         MovePlayer(lastDirection);
     }
-    void HandleDash(bool dash)
+    void HandleDash(Vector2 direction, bool normalDash)
     {
-        if (dash)
+        if (!isDashing && direction != Vector2.zero && dashCount > 0)
         {
-            Dash(lastDirection, false) ; return;
+            if (normalDash)
+            {
+                //Dash(lastDirection, false); 
+                StartCoroutine(DashTp(direction, false));
+                return;
+            }
+            else
+            {
+                //Dash(lastDirection, true);
+                DashTp(direction, true);
+                return;
+            }
         }
-        else 
-        {
-            Dash(lastDirection, true) ; return;
-        }
+        
     }
-
-    System.Collections.IEnumerator Dash(Vector2 direction, bool changeDirection)
+    //borrar
+    void Dash(Vector2 direction, bool changeDirection)
     {
+        Debug.Log("Deberia Dashear");
         if (changeDirection) { direction = -direction; }
         rb.linearVelocity = direction * (speed + dashSpeed);
-        yield return new WaitForSeconds(dashDuration);
         isDashing = false;
     }
 
+    //void DashTp(Vector2 direction, bool changeDirection)
+    //{
+    //    isDashing = true;
+    //    if (changeDirection) { direction = -direction; }
+    //    Vector2 dashTarget = rb.position + direction * dashSpeed;
+    //    Debug.Log($"DashTarget: " + dashTarget);
+
+    //    // Opcional: chequeo de colisiones para no atravesar paredes
+    //    Debug.DrawRay(rb.position, direction * dashSpeed, Color.red, dashSpeed);
+    //    RaycastHit2D hit = Physics2D.Raycast(rb.position, direction, dashSpeed, obstacleLayer);
+    //    if (hit.collider != null)
+    //    {
+    //        dashTarget = hit.point; // Frenar en la pared
+    //        Debug.Log($"DashTarget: " + dashTarget);
+    //    }
+
+    //    rb.MovePosition(dashTarget);
+    //    dashTarget = Vector2.zero;
+    //    Debug.Log($"DashTarget Final: " + dashTarget);
+    //    Debug.Log($"pos: " + rb.position);
+    //    isDashing = false;
+        
+    //}
+    IEnumerator DashTp(Vector2 direction, bool changeDirection)
+    {
+        isDashing = true;
+        if (changeDirection) { direction = -direction; }
+        Vector2 dashTarget = rb.position + direction * dashSpeed;
+        Debug.Log($"DashTarget: " + dashTarget);
+
+        // Opcional: chequeo de colisiones para no atravesar paredes
+        Debug.DrawRay(rb.position, direction * dashSpeed, Color.red, dashSpeed);
+        RaycastHit2D hit = Physics2D.Raycast(rb.position, direction, dashSpeed, obstacleLayer);
+        if (hit.collider != null)
+        {
+            dashTarget = hit.point; // Frenar en la pared
+            Debug.Log($"DashTarget: " + dashTarget);
+        }
+        yield return new WaitForSeconds(0.1f);
+        rb.MovePosition(dashTarget);
+        dashTarget = Vector2.zero;
+        yield return new WaitForSeconds(dashDuration);
+        Debug.Log($"DashTarget Final: " + dashTarget);
+        Debug.Log($"pos: " + rb.position);
+        isDashing = false;
+
+    }
+    //private IEnumerator DashWait()
+    //{
+    //    yield await for return 
+    //}
 }
+

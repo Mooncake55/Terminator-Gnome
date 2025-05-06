62905b30139fd75876b1b90790274d2f5b4f21d3
Martin Gamboa <106704364+ColoGamboa@users.noreply.github.com>
2025-05-06 18:18:06 -0300

    HelthSystem
    copie y pegue lo que hizo grego

---
+++ Terminator-Gnome/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class Health_System : MonoBehaviour
+{
+    public int _maxHealth;
+    public int _actualHealth;
+
+    public Health_System(int maxHealth, int actualHealth)
+    {
+        _maxHealth = maxHealth;
+        _actualHealth = actualHealth;
+    }
+
+    //Heals the entity
+    public void Heal(int amount)
+    {
+        Debug.Log("The entity Heals");
+
+        _actualHealth += amount;
+
+        if (_actualHealth > _maxHealth)
+        {
+            _actualHealth = _maxHealth;
+        }
+    }
+
+    //The entity take damage
+    public void TakeDamage(int damage)
+    {
+        Debug.Log("The entity is Wounded");
+
+        _actualHealth -= damage;
+
+        if (_actualHealth < 0)
+        {
+            Death();
+        }
+    }
+
+    //The entity dies
+    public void Death()
+    {
+        Debug.Log("The entity Dies");
+        _actualHealth = 0;
+    }
+
+    //The entity respawn
+    public void Respawn()
+    {
+        Debug.Log("The entity Arise");
+        _actualHealth = _maxHealth;
+    }
+}
+
